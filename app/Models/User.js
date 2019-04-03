49e1cff1e2c344cac7aaf603fd29c21279612d62
Thauã Borges <thaua97@gmail.com>
2019-04-03 15:55:20 -0300

    :sparkles: Adicionando as Models, Migrations e Controllers dos Objetos Image e Property :foggy: :house:

--- app/Models/User.js
+++ app/Models/User.js
@@ -1,9 +1,6 @@
 'use strict'
 
-/** @type {typeof import('@adonisjs/lucid/src/Lucid/Model')} */
 const Model = use('Model')
-
-/** @type {import('@adonisjs/framework/src/Hash')} */
 const Hash = use('Hash')
 
 class User extends Model {
@@ -34,6 +31,13 @@ class User extends Model {
   tokens () {
     return this.hasMany('App/Models/Token')
   }
+
+
+  properties () {
+    return this.hasMany('App/Models/Property')
+  }
+
+
 }
 
 module.exports = User
